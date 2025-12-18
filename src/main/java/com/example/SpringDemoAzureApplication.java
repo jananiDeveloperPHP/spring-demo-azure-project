ff6d7b337fe821c15b948b6c9be1fdda12b4034b
Janani <jananik312@gmail.com>
2025-12-18 22:21:59 +0530

    Update SpringDemoAzureApplication.java

--- src/main/java/com/example/SpringDemoAzureApplication.java
+++ src/main/java/com/example/SpringDemoAzureApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDemoAzureApplication {
 
+    
+    public String message(){
+        return  "Congrats! youR app deployed successfully in Azure ";
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(SpringDemoAzureApplication.class, args);
     }
